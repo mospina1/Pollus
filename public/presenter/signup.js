c3ff2792a2837fa0983ac8ef2266248f615829e4
Mark Ospina <mark.ospina11@gmail.com>
2024-10-21 14:26:22 -0400

    Added Sign In
    Sign in page now redirects to Login page upon completion of creating an account

--- public/presenter/signup.js
+++ public/presenter/signup.js
@@ -1,19 +1,42 @@
-import {createUserWithEmailAndPassword, getAuth} from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js'
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBlGRwzi9bkETIXnjWPHST3g1sqVkkYCY4",
+    authDomain: "pollus-cs190.firebaseapp.com",
+    projectId: "pollus-cs190",
+    storageBucket: "pollus-cs190.appspot.com",
+    messagingSenderId: "189842869857",
+    appId: "1:189842869857:web:8678b7ac814788ed14e420",
+    measurementId: "G-LWWGNLWPEG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const auth = getAuth();
-const button = document.querySelector("button");
+
+const button = document.getElementById("submit");
 button?.addEventListener("click", clickEvent => {
+
+    // onAuthStateChanged(auth, user => {
+    //     if(user == null) { return;}
+        
+    // });
 
     let email = document.getElementById("username").value;
     let password= document.getElementById("password").value;
     let confirm = document.getElementById("confirmation").value;
 
-    if (password == confirm)
+    if (password === confirm)
     {
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredentials) => {
                 // created account successfully
                 const user = userCredentials.user;
+                alert("Account created");
+                document.location.href = `/presenter/login.html`;
 
             }).catch((error) => {
                 //do something here with error
@@ -27,3 +50,4 @@ button?.addEventListener("click", clickEvent => {
         alert("Passwords don't match");
     }
 });
+
